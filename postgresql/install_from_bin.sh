e473446d95c159b08240a7c685720e9cb38de16b
Finestre <fines3@yandex.ru>
2026-01-18 22:39:05 +0300

    Установка PostgreSQL из бинарников

--- postgresql/install_from_bin.sh
+++ postgresql/install_from_bin.sh
@@ -10,4 +10,10 @@
 # 8. Создать пользователя postgres
 # 9. Создать папку для данных (Data Directory) и выдать права на неё пользователю postgres
 # 10. Инициализировать кластер (команда initdb)
-# 11. Запустить сервер (команда pg_ctl).
+# 11. Запустить сервер (команда pg_ctl)
+
+# 1. Устанавливаем зависимости и билиотеки (здесь только самое необходимое)
+
+sudo apt-get update
+sudo apt-get install build-essential libreadline-dev zlib1g-dev
+sudo apt-get install libssl-dev libicu-dev libsystemd-dev pkg-config
